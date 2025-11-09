292fdffac28d63e56a46e65e1fee3aaeab7cbab3
Mayra <127692561+MayLo7@users.noreply.github.com>
2025-11-09 00:57:11 -0300

    fix

--- viandas-api/src/validators/create.user.js
+++ viandas-api/src/validators/create.user.js
@@ -5,8 +5,8 @@ const createUserSchema = Joi.object({
     username: Joi.string().alphanum().min(3).max(15).required(),
     password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
     email: Joi.string().email().required(),
-    plan: Joi.any(),
-    orderCount: Joi.any()
+    plan: Joi.any().strip(),
+    orderCount: Joi.any().strip()
 }); 
 
 module.exports = { createUserSchema };
